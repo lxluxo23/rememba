0710544e6bbeb0475d7f6c8f325389cc6282799f
Luis cespedes <lxluxo23@gmail.com>
2024-06-23 20:07:17 -0400

    avance
    - se añade componente de contacto
    - se añade componente de login
    - se añade componente de registro

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
+import { DialogService, DynamicDialogRef } from 'primeng/dynamicdialog';
+import { ScrollService } from 'src/app/services/scroll.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
-export class HeaderComponent implements OnInit{
+export class HeaderComponent{
 
+  constructor(
+    private router : Router,
+    private scrollService : ScrollService,
+    private ref: DynamicDialogRef,
+    public dialogService: DialogService,
   
-  ngOnInit() {
-    
+  ) { }
+
+  scrollToSection(seccion: string){
+    this.scrollService.scrollTo(seccion)
+  }
+
+  openContact(){
+    console.log(" [ abriendo contacto ] " )
+    this.router.navigate(['/contacto']) 
+
+    // this.ref = this.dialogService.open(ContactoComponent, {
+    //     header: 'Contacto',
+    //     styleClass:'responsive-modal',
+    //     autoZIndex: true,
+    //     width: '60vw',
+    //     modal: true,
+    //     breakpoints: {
+    //       '960px': '75vw',
+    //       '640px': '90vw'
+    //   },
+    // });
   }
 
 }
